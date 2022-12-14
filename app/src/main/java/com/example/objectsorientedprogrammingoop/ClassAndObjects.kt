38a48bf3991cccc37a93adfa8eec8527c90c5d07
julia-martins <juliamfbrasileiro21@gmail.com>
2022-12-14 14:01:37 -0300

    Changes done

--- app/src/main/java/com/example/objectsorientedprogrammingoop/ClassAndObjects.kt
+++ app/src/main/java/com/example/objectsorientedprogrammingoop/ClassAndObjects.kt
@@ -4,52 +4,53 @@ import kotlin.math.floor
 
 data class User(val id: Long, var name: String)
 
-fun  main(){
+fun  main() {
 // region Array
 //    val numbers = intArrayOf(21, 72, 81)
 //    val numbers = arrayOf(27, 79, 92)
-    val numbers = doubleArrayOf(27.1, 79.4, 92.0)
+//    val numbers = doubleArrayOf(27.1, 79.4, 92.0)
 //    println("Initial values ${numbers.contentToString()}")
 //    for(element in numbers){
 //        println("${element +7}")
 //    }
-    numbers[0] = 21.7
-    numbers[1] = 72.8
-    numbers[2] = 81.9
+//    numbers[0] = 21.7
+//    numbers[1] = 72.8
+//    numbers[2] = 81.9
 //    println("Latest values ${numbers.contentToString()}")
 
-    val mix = arrayOf("Julia", "Brasileiro", 20, "2002/09/21", 20, 0.00, "Developer")
+//    val mix = arrayOf("Julia", "Brasileiro", 20, "2002/09/21", 20, 0.00, "Developer")
 
 //    println(mix.contentToString())
 
-    val fruits = arrayOf(fruit("Strawberry", 10.96), fruit("Sleeve", 8.90))
-    for(index in fruits.indices){
+//    val fruits = arrayOf(fruit("Strawberry", 10.96), fruit("Sleeve", 8.90))
+//    for (index in fruits.indices) {
 //        println("${fruits[index].name} is in index $index")
-    }
+//    }
 
-    for(fruit in fruits){
+//    for (fruit in fruits) {
 //        println("${fruit.name}")
-    }
+//    }
 
 // endregion
-// region Lists
+
+// region Lists Part 1
 //    val stringList: List<String> = listOf("Julia", "Thais", "Lucas", "Bruce", "Barbara")
 //    val mixedTypeList: List<Any> = listOf("Julia", 21, 9, "Birthday", 101.0, "Weights", "Kg")
-
-//    for(value in mixedTypeList){
+//
+//    for (value in mixedTypeList) {
 //        when (value) {
-//        is Int -> {
-//            println("Integer: '$value'")
-//        }
-//        is Double -> {
-//            println("Double: '$value' with Floor value ${floor(value)}")
-//        }
-//        is String -> {
-//            println("String: '$value' f length ${value.length}")
-//        }
-//        else -> {
-//            println("Unknown Type")
-//        }
+//            is Int -> {
+//                println("Integer: '$value'")
+//            }
+//            is Double -> {
+//                println("Double: '$value' with Floor value ${floor(value)}")
+//            }
+//            is String -> {
+//                println("String: '$value' f length ${value.length}")
+//            }
+//            else -> {
+//                println("Unknown Type")
+//            }
 //         Alternatively
 //        if(value is Int){
 //            println("Integer: $value")
@@ -61,24 +62,79 @@ fun  main(){
 //            println("Unknown Type")
 //        }
 //        }
-//    }
-//    val obj1: Any = "Wolf and Seals are the best animals"
-//    if(obj1 !is String){
-//        println("Not a String")
-//    }else{
-//        println("Found a String of Length ${obj1.length}")
-//    }
+//        }
+//        val obj1: Any = "Wolf and Seals are the best animals"
+//        if (obj1 !is String) {
+//            println("Not a String")
+//        } else {
+//            println("Found a String of Length ${obj1.length}")
+//        }
 //
-//    val str1: String = obj1 as String
-//    println(str1.length)
+//        val str1: String = obj1 as String
+//        println(str1.length)
 //
-//    val obj2: Any = 2172
-//    val str2: String? = obj2 as? String
-//    println(str2)
+//        val obj2: Any = 2172
+//        val str2: String? = obj2 as? String
+//        println(str2)
+//
+//        val obj3: Any = 8172
+//        val str3: String? = obj3 as? String
+//        println(str3)
+//    }
+// endregion Par
 
-//    val obj3: Any = 8172
-//    val str3: String? = obj3 as? String
-//    println(str3)
+// region List Part 2 and Mutable List
+//    val months = listOf("January", "February", "March", "April", "May", "Jun", "July", "August")
+//    val anuTypes = listOf("Julia", "Brasileiro", 20, true)
+//
+//    val additionalMonths = months.toMutableList()
+//    val newMonths = arrayOf("September", "October", "November", "December")
+//
+//    additionalMonths.addAll(newMonths)
+//    additionalMonths.add("Wolf")
+//
+//    println(additionalMonths)
+//
+//    val days = mutableListOf<String>("Mon", "Tue", "Wed", "Thu", "Fry")
+//
+//    days.add("Sun")
+//    days.add(5, "Sat")
+//    days.add(2, "Gargamel")
+//    println(days)
+//    days.removeAt(2)
+//    println(days)
+
+//    val removeList = mutableListOf<String>("Mon", "Tue")
+//    days.removeAll(days)
+//    println(days)
+// endregion
+
+// region Set and Maps
+    val fruits = setOf("Dragon Fruit", "Boysenberry", "Grape", "Dragon Fruit")
+    println(fruits.toSortedSet())
+
+    val newFruits = fruits.toMutableList()
+    newFruits.add("Mango")
+    newFruits.add("Strawberry")
+    //println(newFruits.toSortedSet())
+
+    val daysOfTheWeek = mapOf(1 to "Monday", 2 to "Tuesday", 3 to "Wednesday")
+
+    for(key in daysOfTheWeek.keys){
+        println("$key is to ${daysOfTheWeek[key]}")
+    }
+
+    val Fruits = mapOf("Not so good" to fruit("Grape", 2.6), "I don't like" to fruit("Water Mellow", 3.9), "Favorite" to fruit("Tangerine", 2.1))
+
+    val newDaysOfWeek = daysOfTheWeek.toMutableMap()
+    newDaysOfWeek[4] = "Thursday"
+    newDaysOfWeek[5] = "Friday"
+    newDaysOfWeek[6] = "Saturday"
+    newDaysOfWeek[7] = "Sunday"
+
+    println(newDaysOfWeek.toSortedMap())
+
+    println(Fruits.values)
 // endregion
 
 //     region Function USER
@@ -139,7 +195,7 @@ fun  main(){
 //    myVehicle.brake()
 //    myVehicleElectric.brake()
 
-    // Polymorphism
+        // Polymorphism
 //    myVehicle.drive(200.0)
 //    myVehicleElectric.drive(200.0)
 
@@ -156,10 +212,7 @@ fun  main(){
 //    wolf.breath()
 // endregion
 }
-
-data class fruit(val name: String, val price: Double){
-
-}
+data class fruit(val name: String, val price: Double)
 
 // region Class Car
 //class Car(){
